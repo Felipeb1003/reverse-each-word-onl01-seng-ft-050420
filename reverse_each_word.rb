7b6691d05beb7af7d92a3d4064d56445c7973327
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-09 17:43:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 
 def reverse_each_word(phrase)
    new_array = phrase.split(" ")
-   new_array.each do |phrases|
-    puts phrases.reverse
+   new_array.each.reverse do |phrases|
+    puts phrases
    end
    
   end
